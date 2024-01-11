136fe485769caa8f94bf6ad5bcc32bac744cc8f6
Robert Shelton <sheltrob000@warren.k12.in.us>
2024-01-11 10:39:01 -0500

    try catch a ball you uncoridanated human

--- 04_Try Catch Finally/Assignment/src/App.java
+++ 04_Try Catch Finally/Assignment/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("type first number");
+        int number1 = scanner.nextInt();
+        System.out.println("type the second number");
+        int number2 = scanner.nextInt();
+        
+        try {
+            int divided = number1 / number2;
+            System.out.println(divided);
+        }catch(ArithmeticException e) {
+            System.out.println("you tried to divide by 0 you dummy");
+        }
+        scanner.close();
+
+
     }
 }
