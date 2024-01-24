1c51c29c7b261818e9b28923f00c6b533642ccc5
Robert Shelton <sheltrob000@warren.k12.in.us>
2024-01-24 10:37:40 -0500

    generil

--- 02_01_Generics/Assignment/src/App.java
+++ 02_01_Generics/Assignment/src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Property<String> myStringProperty = new Property<String>();
+        myStringProperty.set("Hi");
+        System.out.println(myStringProperty.get());
+        myStringProperty.set("Hello");
+        System.out.println(myStringProperty.get());
+
+        Property<Integer> myInteger = new Property<Integer>();
+        myInteger.set(3);
+        System.out.println(myInteger.get());
+        myInteger.set(2);
+        System.out.println(myInteger.get());
+
+        Property<Boolean> myBooleanProperty = new Property<Boolean>();
+        myBooleanProperty.set(true);
+        System.out.println(myBooleanProperty.get());
+        myBooleanProperty.set(false);
+        System.out.println(myBooleanProperty.get());
+
     }
 }
