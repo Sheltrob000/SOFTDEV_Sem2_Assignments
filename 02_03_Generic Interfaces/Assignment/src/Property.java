09a5491dcd97f051555ea76e527aaf1edb49cd5e
Robert Shelton <sheltrob000@warren.k12.in.us>
2024-01-25 10:41:08 -0500

    is equal?

---
+++ 02_03_Generic Interfaces/Assignment/src/Property.java
@@ -0,0 +1,67 @@
+public class Property<T extends Comparable<T>> implements Inequality<T> {
+
+    private T t;
+
+    public Property(T t) {
+        this.t = t;
+    }
+
+    public Property() {
+        this.t = null;
+    }
+
+    public T get() {
+        return this.t;
+    }
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    @Override
+    public boolean isGreaterThan(Property<T> other) {
+        if (t.compareTo(other.t) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isLessThan(Property<T> other) {
+        if (t.compareTo(other.t) == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isEqual(Property<T> other) {
+        return isEqual(other);
+    }
+
+    @Override
+    public boolean isNotEqual(Property<T> other) {
+        return !isEqual(other);
+    }
+
+    @Override
+    public boolean isGreaterThanOrEqual(Property<T> other) {
+        if (t.compareTo(other.t) == 1 || isEqual(other)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isLessThanOrEqual(Property<T> other) {
+        if (t.compareTo(other.t) == -1 || isEqual(other)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
