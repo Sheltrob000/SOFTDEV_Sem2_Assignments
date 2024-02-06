0cad84d198bc1a94c4ea001cdd4c67cabae1344b
Robert Shelton <sheltrob000@warren.k12.in.us>
2024-02-06 10:46:26 -0500

    readfile

--- 03_File Handling/02_Reading a File/Assignment/src/App.java
+++ 03_File Handling/02_Reading a File/Assignment/src/App.java
@@ -1,5 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String fileName = "src/file.txt";
+
+
+        Path filePath = Paths.get(fileName);
+        List<String> lines = Files.readAllLines(filePath);
+
+        for (int numlines = 0; numlines <= lines.size(); numlines++){
+            System.out.println(lines.get(numlines));
+        }
+
+        // Loop through the list of lines
+
     }
 }
