2c26ab424ef7edecd78294a949c24a56b51587f0
Robert Shelton <sheltrob000@warren.k12.in.us>
2024-02-23 10:51:52 -0500

    andthen

--- 04_Functional Programming_Streams/03_Optionals/Assignment/src/App.java
+++ 04_Functional Programming_Streams/03_Optionals/Assignment/src/App.java
@@ -3,22 +3,23 @@ import java.util.function.Function;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //TODO:  make a function called subtract5 using lambda notation
-        //TODO: make a function called times5 using lambda notation
-        //TODO: make a function called subtract5Times5 using andThen
+        Function<Integer, Integer> subtract5 = (num) -> num - 5;
+        Function<Integer, Integer> times5 = (num) -> num * 5;
+        Function<Integer, Integer> subtract5Times5 = subtract5.andThen(times5);
 
-        //TODO:  create an int called testNumber set to 1;
-        //TODO:  apply subtract5Times5 to testNumber
+
+        int testNumber = 1;
+        subtract5Times5.apply(testNumber);
 
 
         //Lambda notation examples
         Function<Integer, Integer> add5 = (num) -> num + 5;
         Function<Integer, Integer> div2 = (num) -> num / 2;
-        // Function<Integer, Integer> add5Divide2 = (num) -> {
-        //     num = num + 5;
-        //     num = num / 5;
-        //     return num;
-        // };
+        Function<Integer, Integer> add5Divide2 = (num) -> {
+            num = num + 5;
+            num = num / 5;
+            return num;
+        };
 
         Function<Integer, Integer> add5Divide2UsingAndThen = add5.andThen(div2);
 
