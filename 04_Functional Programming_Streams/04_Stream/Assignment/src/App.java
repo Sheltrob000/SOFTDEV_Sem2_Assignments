cf6448b0bcf4abb704df9f8d140c0dc50dd06520
Robert Shelton <sheltrob000@warren.k12.in.us>
2024-03-12 08:57:21 -0400

    streams

--- 04_Functional Programming_Streams/04_Stream/Assignment/src/App.java
+++ 04_Functional Programming_Streams/04_Stream/Assignment/src/App.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        List<Integer> myList = List.of(1, 2, 3);
+        // List<Integer> myList = List.of(1, 2, 3);
 
-        Integer cumulativeSum = myList.stream().reduce(0, (a, b) -> a + b );
-        System.out.println(cumulativeSum);
+        // Integer cumulativeSum = myList.stream().reduce(0, (a, b) -> a + b );
+        // System.out.println(cumulativeSum);
         
-        Integer cumulativeProduct = myList.stream().reduce(1, (a, b) -> a * b);
+        // Integer cumulativeProduct = myList.stream().reduce(1, (a, b) -> a * b);
 
         //create  a list of strings.  
         //fill that list with your daily school schedule
@@ -17,6 +20,21 @@ public class App {
         //map your list of strings to another list of strings using 
         //.stream().map(toLowerCase).toList(); and set to a variable called lowerCaseSchedule
         //System.out.println(your lower cased list)
+        List<String> string = new ArrayList<String>();
+        string.add("English");
+        string.add("Webdev");
+        string.add("Phycics");
+        string.add("Softdev");
+        string.add("Precal");
+        string.add("History");
+        string.add("Art");
+
+        Function<String, String> tolowercase = (s) -> s.toLowerCase();
+
+        Stream<String> classstream = string.stream();
+        var lowercaseClassList = classstream.map(tolowercase).toList();
+        System.out.print(lowercaseClassList);
+
 
 
     }
